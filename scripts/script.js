231b881f7c91f847d466d7c16e5bf15773c36122
sylvestertarnowski <sylwek.tarnowski@gmail.com>
2018-11-11 13:25:23 +0100

    Smooth scrolling to navigation anchors

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
 console.log("Connected");
+
+$('a').on('click', function (e) {
+    console.log($(this).attr("href"));
+
+    e.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 600, 'swing');
+});
+
+
+
